8d5b72ce5c61a157c9603058e24e9cdb46001e63
lps08 <lucianolps08@gmail.com>
2021-08-05 17:12:46 -0300

    firebase initial configs

---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import 'firebase/auth';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDSZZ9eRl3yHCSFMb8buWAibC2zEeBfcCQ",
+    authDomain: "sistema-chamado-d69ba.firebaseapp.com",
+    projectId: "sistema-chamado-d69ba",
+    storageBucket: "sistema-chamado-d69ba.appspot.com",
+    messagingSenderId: "878083667215",
+    appId: "1:878083667215:web:4162c4be9e668f273f7a7a",
+    measurementId: "G-ZT2FDK14LG"
+  };
+
+// check if have connection already started
+if (!firebase.apps.length) {
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
