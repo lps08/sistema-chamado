f22a632bf174493ba7c8f9f0a8b7aef6c41ec4cf
lps08 <lucianolps08@gmail.com>
2021-08-07 13:09:45 -0300

    update user profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -4,16 +4,89 @@ import Header from '../../components/Header';
 import Title from '../../components/Title';
 import avatar from '../../assets/avatar.png';
 import { FiSettings, FiUpload } from 'react-icons/fi';
+import firebase from '../../services/firebaseConnection';
 
 import { AuthContext } from '../../contexts/auth';
 
 export default function Profile() {
 
-    const { user, signOut } = useContext(AuthContext);
+    const { user, signOut, setUser, storageUser } = useContext(AuthContext);
 
     const [ nome, setNome ] = useState( user && user.nome );
     const [ email, setEmail ] = useState( user && user.email );
     const [ avatarUrl, setAvatarUrl ] = useState( user && user.avatarUrl );
+    const [ imageAvatar, setImageAvatar] = useState(null);
+
+    // busca a imagem e setta para a variavel imagemAvatar e setAvatarUrl
+    function handleFile(e) {
+        if (e.target.files[0]) {
+            const image = e.target.files[0];
+
+            if (image.type === 'image/jpeg' || image.type === 'image/png') {
+                setImageAvatar(image);
+                setAvatarUrl(URL.createObjectURL(image))
+            }else {
+                alert('Permitido imagem .jpeg e .png, apenas.');
+                setImageAvatar(null);
+                return null;
+            }
+        }
+    }
+
+    async function handleUploadImage() {
+        const upTask = await firebase.storage()
+        .ref(`images/${user.uid}/${imageAvatar.name}`)
+        .put(imageAvatar)
+        .then( async () => {
+            console.log('Foto enviada com sucesso.');
+
+            // buscando url da imagem
+            await firebase.storage().ref(`images/${user.uid}`)
+            .child(imageAvatar.name).getDownloadURL()
+            .then( async (url) => {
+                // apos enviar a foto, busca a url para salvar no firestore do perfil do usuario
+                await firebase.firestore().collection('users')
+                .doc(user.uid)
+                .update({
+                    avatarUrl: url,
+                    nome: nome
+                })
+                .then(() => {
+                    // atualiza o estado com novas informações, assim como o starage
+                    let data = {
+                        ...user,
+                        avatarUrl: url,
+                        nome: nome
+                    }
+                    setUser(data);
+                    storageUser(data);
+                })
+            })
+        })
+    }
+
+    async function handleSave(e) {
+        // previne para não recarregar a pagina
+        e.preventDefault()
+
+        if (imageAvatar === null && nome !== '') {
+            await firebase.firestore().collection('users')
+            .doc(user.uid)
+            .update({
+                nome: nome
+            })
+            .then(() => {
+                let data = {
+                    ...user,
+                    nome: nome
+                };
+                setUser(data);
+                storageUser(data);
+            })
+        } else if (imageAvatar !== null && nome !== '') {
+            handleUploadImage();
+        }
+    }
 
     return (
         <div>
@@ -26,13 +99,13 @@ export default function Profile() {
                 </Title>
 
                 <div className="container">
-                    <form className="form-profile">
+                    <form className="form-profile" onSubmit={ handleSave }>
                         <label className="label-avatar">
                             <span>
                                 <FiUpload color="#fff" size={25} />
                             </span>
 
-                            <input type="file" accept="image/*" />
+                            <input type="file" accept="image/*" onChange={ handleFile } />
                             { avatarUrl === null ?
                                 <img src={avatar} width="250" height="250" alt="Avatar user" />
                                 :
--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage';
 
 var firebaseConfig = {
     apiKey: "AIzaSyDSZZ9eRl3yHCSFMb8buWAibC2zEeBfcCQ",
