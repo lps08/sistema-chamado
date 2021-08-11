0bc9edbc02904140533ac1944a0fc4c3b97be2d0
lps08 <lucianolps08@gmail.com>
2021-08-11 12:48:56 -0300

    getting chamdos from database

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,14 +1,74 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import './dashborad.css'
 
 import Header from "../../components/Header";
 import Title from '../../components/Title';
 import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from 'react-icons/fi'
 import { Link } from "react-router-dom";
+import { format } from 'date-fns';
+
+import firebase from '../../services/firebaseConnection';
 
 export default function Dashboard() {
     // const { signOut } = useContext(AuthContext);
-    const [chamados, setChamados] = useState([1]);
+    const [chamados, setChamados] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [loadingMore, setLoadingMore] = useState(false);
+    const [isEmpty, setIsEmpty] = useState(false);
+    const [lastDoc, setLastDoc] = useState();
+
+    useEffect(() => {
+        loadChamados();
+        // quando o componente for desmontado
+        return () => {
+
+        }
+    });
+
+    async function loadChamados() {
+        await firebase.firestore().collection('chamados').orderBy('crated', 'desc').limit(5)
+        .get()
+        .then((snapshot) => {
+            updateState(snapshot);
+        })
+        .catch((err) => {
+            console.log('Error: ' + err);
+            setLoadingMore(false);
+        })
+
+        setLoading(false);
+    }
+
+    async function updateState(snapshot) {
+        const isCollectionEmpty = snapshot.size === 0;
+
+        if (!isCollectionEmpty) {
+            let lista = [];
+
+            snapshot.forEach((doc) => {
+                lista.push({
+                    id: doc.id,
+                    assunto: doc.data().assunto,
+                    clienteId: doc.data().clienteId,
+                    cliente: doc.data().cliente,
+                    // create == created - escrito errado
+                    created: doc.data().crated,
+                    createdFormated: format(doc.data().crated.toDate(), 'dd/MM/yyyy'),
+                    status: doc.data().status,
+                    complemento: doc.data().complemento
+                });
+            });
+
+            const lastDoc = snapshot.docs[snapshot.docs.length - 1]; // pegando os ultimos documentos
+            setChamados(chamados => [...chamados, ...lista]);
+            setLastDoc(lastDoc);
+            
+        } else {
+            setIsEmpty(true);
+        }
+
+        setLoadingMore(false);
+    }
 
     return(
         <div>
